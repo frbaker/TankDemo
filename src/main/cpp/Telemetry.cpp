f35abb20c04b301d56331037c0cfe649163e2357
Mariner Robotics <marinerrobotics3267@gmail.com>
2023-01-28 12:03:37 -0600

    Implemented robot metric snapshot capturer

--- src/main/cpp/Telemetry.cpp
+++ src/main/cpp/Telemetry.cpp
@@ -6,6 +6,7 @@ Telemetry::Telemetry(DriveTrain *drvt)
     gyro = new ctre::phoenix::sensors::PigeonIMU(16); // Create our gyro object with can ID 16
     gyro->SetYaw(0.0);                                // Set the x angle to 0
     drivetrain_data = new SparkMaxPacket;             // Allocate memory for the packet struct
+    snapshot_timer = new Timer(100);                  // Set the timer to take a snapshot every 100ms(10x second)
 }
 
 SparkMaxPacket *Telemetry::exportTelemetry()
@@ -15,13 +16,27 @@ SparkMaxPacket *Telemetry::exportTelemetry()
 
 void Telemetry::runMetrics()
 {
-    // Update our speed and position data
-    drivebase->updateTelemetry();
+    if(snapshot_timer->getTimer()){//Capture a snapshot of the robots data every 100ms
+        captureSnapshot();
+    }
     // Do calculations and such here
+}
+
+void Telemetry::captureSnapshot()
+{
+    drivebase->updateTelemetry();
+    struct Snapshot *temp = new Snapshot;
+    temp->left_pos = drivetrain_data->encoder_position_1 * drivetrain_data->encoder_position_2 / 2;  // Average data values
+    temp->right_pos = drivetrain_data->encoder_position_3 * drivetrain_data->encoder_position_4 / 2; // Average data values
+    temp->left_speed = drivetrain_data->motor_power_1 * drivetrain_data->motor_power_2 / 2;          // Average left speed value
+    temp->right_speed = drivetrain_data->motor_power_3 * drivetrain_data->motor_power_4 / 2;         // Average right speed value
+    temp->x_rot = gyro->GetYaw();                                                                    // Get x rotation
+    instruction_stack.push(temp);                                                                    // Add snapshot struct to temp
 }
 
 Telemetry::~Telemetry()
 {
     delete gyro;            // Free the gyro memory
     delete drivetrain_data; // Free the telemetry struct
+    delete snapshot_timer;  // Free timer
 }
--- src/main/include/Telemetry.h
+++ src/main/include/Telemetry.h
@@ -4,7 +4,8 @@
 #include "DriveTrain.h"
 #include <ctre/phoenix/sensors/PigeonIMU.h>
 #include "DataPacket.h"
-#
+#include "Timer.h"
+#include <queue>
 
 class Telemetry
 {
@@ -16,9 +17,22 @@ public:
     void runMetrics();                 // Main function for updating data
 
 private:
+    void captureSnapshot();
+
     DriveTrain *drivebase; // Object pointer to hold reference to our drive base
     ctre::phoenix::sensors::PigeonIMU *gyro;
     SparkMaxPacket *drivetrain_data; // Holds the robot telemetry data
+    Timer *snapshot_timer;           // Timer for managing snapshots of robo data
+    struct Snapshot
+    {
+        double left_pos;
+        double left_speed;
+        double right_pos;
+        double right_speed;
+        double x_rot;
+    };
+
+    std::queue<struct Snapshot *> instruction_stack;
 };
 
 #endif // TELEMETRY_H
